b3f70a332e567119ac03b4ec7b0946cab4f196e5
SunnyStarscout2 <153956700+SunnyStarscout2@users.noreply.github.com>
2023-12-15 22:43:33 +0100

    Add files via upload
    A few lines of code i imagine the swedish armed forces use when deciding whether or not to send someone to evaluation for military service

---
+++ Försvarsmakten.py
@@ -0,0 +1,10 @@
+x = input("har du en fastställd autismdiagnos? ")
+
+if x == "ja":
+    print("din ansökan är avskriven")
+elif x == "nej":
+    print("din ansökan är antagen")
+else:
+    print("ditt svar måste vara \"ja\" eller \"nej\"")
+
+
